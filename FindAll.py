1ea2bf231d310c760979b31e9a1658e013a0010a
Jasilb <jsilbige@mail.yu.edu>
2021-06-29 17:52:46 -0400

    small fix

--- FindAll.py
+++ FindAll.py
@@ -20,13 +20,12 @@ def myprint(d):
                 v= str(v.tolist())
             if isinstance(v, bytes):
                 v= v.decode("utf-8")
-            dataSet.write(("{0} : {1}".format(k, v)))
+            dataSet.write(("{0}:{1}".format(k, v)))
             dataSet.write("\n")
     
 ds, info = tfds.load('coco', split='train', with_info=True)
 ds = tfds.as_numpy(ds)
-file = open(
-    r"C:\Users\jakes\tensorflow_datasets\coco\2014\1.1.0\objects-label.labels.txt")
+file = open(r"C:\Users\jakes\tensorflow_datasets\coco\2014\1.1.0\objects-label.labels.txt")
 word = file.readlines()
 objects = ["bicycle", "motorcycle", "bus", "truck", "car", "train",
            "person", "traffic light", "stop sign", "fire hydrant"]
@@ -40,22 +39,21 @@ for obj in objects:
 
 
 x = 0
+
 # remove breakpoint to continue though whole dataset
 for example in ds:  # (image[], labels[], objects[], bbox[])
+    
     if x == 7:
         break
 
-    image = example['image']
+    image =example['image']
     labels = example['objects']['label']
     bboxes = example['objects']['bbox']
-    print(labels)
+    #print(labels)
     # print(bboxes)  # [y_min, x_min, y_max, x_max]
     if (set(lines) & set(labels)):
         myprint(example)      
     x += 1
 
-
-file.close
-dataSet.close
 
 
