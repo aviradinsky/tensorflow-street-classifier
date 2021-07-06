4f2640f10b2f640ac26013dc1a7a74f3b3e56909
Ubuntu <ubuntu@ip-172-31-13-16.ec2.internal>
2021-07-06 01:37:39 +0000

    changed model to all dense layers

--- model1.py
+++ model1.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
-from tensorflow.python.keras.layers.core import Dropout
+from tensorflow.python.keras.layers.core import Dense, Dropout, Flatten
 # %%
 DATASET_PATH = f'{os.getcwd()}/instances/'
 print(DATASET_PATH)
@@ -21,7 +21,7 @@ train_data=tf.keras.preprocessing.image_dataset_from_directory(
     color_mode='rgb',
     validation_split=0.1,
     subset='training',
-    seed=613
+    seed=6
 )
 
 validate_data=tf.keras.preprocessing.image_dataset_from_directory(
@@ -31,21 +31,21 @@ validate_data=tf.keras.preprocessing.image_dataset_from_directory(
     color_mode='rgb',
     validation_split=0.1,
     subset='validation',
-    seed=613
+    seed=6
 )
 # %%
 num_classes = 10
 
 model = Sequential([
   layers.experimental.preprocessing.Rescaling(1./255, input_shape=(256, 256, 3)),
-  layers.Conv2D(16, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(32, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Conv2D(64, 3, padding='same', activation='relu'),
-  layers.MaxPooling2D(),
-  layers.Flatten(),
-  layers.Dropout(0.2),
+  layers.Flatten(input_shape=(256,256,3)),
+  layers.Dense(256,activation='relu'),
+  layers.Dense(256,activation='relu'),
+  layers.Dense(256,activation='relu'),
+  layers.Dense(256,activation='relu'),
+  layers.Dense(256,activation='relu'),
+  layers.Dense(256,activation='relu'),
+  layers.Dense(256,activation='relu'),
   layers.Dense(128, activation='relu'),
   layers.Dense(num_classes)
 ])
@@ -53,6 +53,21 @@ model = Sequential([
 model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
+# %%
+i = 1
+for j in train_data:
+  print(type(j[0]))
+  if i < 4407:
+    i += 1
+    print(i)
+    continue
+  else:
+    plt.figure()
+    plt.imshow(j)
+    plt.colorbar()
+    plt.grid(False)
+    plt.show()
+    break
 # %%
 epochs=1
 history = model.fit(
