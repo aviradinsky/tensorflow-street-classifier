0f610b34a8ed1ebc8b0efa0177c35197a46ddcbd
aviradinsky <avi.radinsky@gmail.com>
2021-07-11 14:34:40 +0000

    every 8th pic goes into the training set

--- model_v2.py
+++ model_v2.py
@@ -1,15 +1,14 @@
 # %%
+from re import S
 import tensorflow as tf
+from tensorflow.python.ops.math_ops import count_nonzero
 import tensorflow_datasets
 import os
 # %%
-d = tensorflow_datasets.load('coco').get('test')
-thing = None
-t = d.take(100)
-for i in t:
-    print(i.get('objects').get('label'))
-#%%
-
+"""
+global variables
+"""
+directory = f'{os.getcwd()}/data/'
 chosen_labels = [
     1,  # bike
     3,  # motorcycle
@@ -23,7 +22,14 @@ chosen_labels = [
     10  # fire hydrant
 ]
 # %%
+
 def crop_tensor_by_nth_bbox(tensor, nth_bbox):
+    """
+    given an image with bboxs this method returns the cropped image
+    for the nth bbox
+    """
+
+
     bbox = tensor.get('objects').get('bbox').numpy()[nth_bbox]
     image = tensor.get('image')
 
@@ -37,60 +43,94 @@ def crop_tensor_by_nth_bbox(tensor, nth_bbox):
     target_height = int(bottom_line - top_line)
     target_width = int(right_line - left_line)
 
+    """
     if target_height == 0 or target_width == 0 or top_line == 0 or right_line == 0:
         print('garbage is going in')
-
-    cropped_image = tf.image.crop_to_bounding_box(image,
+    """
+    return tf.image.crop_to_bounding_box(image,
                                                     offset_height=offset_height,
                                                     offset_width=offset_width,
                                                     target_height=target_height,
                                                     target_width=target_width
                                                     )
-    
-    return cropped_image
+    pass
 
-def load_data(key: str) -> tf.data.Dataset:
+def slice(image) -> list:
+    x,y,z = image.shape
+    half = int(x / 2)
 
-    directory = f'{os.getcwd()}/data/{key}'
+    a = half - 3
+    b = half + 3
+    first_half = tf.slice(image, [0, 0, 0], [a, y, z])
+    second_half = tf.slice(image, [a, 0, 0], [b, y, z])
+    return [first_half,second_half]
+
+def slice_into_4ths(image):
+    return slice(slice(image)[0]) + slice(slice(image)[1])
+
+def get_image_manipulations_and_path(image,count) -> list:
+    tensor = image
+    objects = image.get('objects')
+    all_labels = objects.get('label').numpy()
+    image = image.get('image') 
+    ret = []
+    path_buffer: str= ''
+    if len(set(all_labels) & set(chosen_labels)) == 0:
+        """
+        this is background and will be randomly cropped to increase number
+        """
+        images = slice_into_4ths(image)
+        for i in images:
+            if count % 8 == 0:
+                path = f'{directory}/test/10/{count}.jpeg'
+            else:
+                path = f'{directory}/10/{count}.jpeg'
+            count += 1
+            ret.append((i,path))
+    else:
+        """
+        this is images that have objects we want, they will be cropped
+        """
+        for ind, label in enumerate(all_labels):
+            if label in chosen_labels:
+                image = crop_tensor_by_nth_bbox(tensor,ind)
+                if count % 8 == 0:
+                    path = f'{directory}/test/{chosen_labels.index(label)}/{count}.jpeg'
+                else:
+                    path = f'{directory}/{chosen_labels.index(label)}/{count}.jpeg'
+                count += 1
+                ret.append((image,path))
+    return (ret,count)
+
+
+def set_data_in_directories() -> None:
+    """
+    checking to see if the path ('data/' and 'data/test') exists, if it does no code will run
+    """
 
     if os.path.exists(f'{directory}/0'):
         # this means that the data was already loaded
-        print(f'Data of type {key} is already in the data folder')
+        print(f'Data is already in the data folders')
+        return
     else:
         for i in range(len(chosen_labels) + 1):
             os.makedirs(f'{directory}/{i}')
-        count = 0
-        dataset = tensorflow_datasets.load('coco').get(key)        
-        for data in dataset:
-            image = data.get('image')
-            objects = data.get('objects')
-            all_labels = objects.get('label').numpy()
-            if len(set(all_labels) & set(chosen_labels)) == 0:
-
-                tf.keras.preprocessing.image.save_img(f'{directory}/{len(chosen_labels)}/{count}.png',image)
-                count += 1
-                continue
-            else:
-                for ind, label in enumerate(all_labels):
-                    if label in chosen_labels:
-                        image = crop_tensor_by_nth_bbox(data,ind)
-                        tf.keras.preprocessing.image.save_img(f'{directory}/{chosen_labels.index(label)}/{count}.png',image)
-                        count += 1
-
-
+            os.makedirs(f'{directory}/test/{i}')
     
-    return tf.keras.preprocessing.image_dataset_from_directory(
-        directory=directory,
-        labels='inferred',
-        label_mode='int',
-        color_mode='rgb',
-        seed=6
-    )
+    """ 
+    there are 4 keys but test and test2015 dont have labels
+    """
 
 
-valid_keys = ('train','validation','test2015')
+    valid_keys = ('train','validation')
+    count = 0
+    for key in valid_keys:
+        for image in tensorflow_datasets.load('coco',shuffle_files=True).get(key):
+            data = get_image_manipulations_and_path(image,count)
 
-test_data = load_data(valid_keys[2])
-
-
+            for manipulation, path in data[0]:
+                tf.keras.preprocessing.image.save_img(path, manipulation)
+                count = data[1] 
+    pass
 # %%
+set_data_in_directories()
