3608729cde36dbad02fc5c6eb3a123ecdb2adab3
Jacob Silbiger <jsilbige@mail.yu.edu>
2021-07-16 17:17:02 +0000

    updated with new size

--- testmodel.py
+++ testmodel.py
@@ -11,6 +11,7 @@ from numpy import random
 import numpy as np
 from PIL import Image, ImageDraw
 import scale_and_slide
+import params
 
 
 # %%
@@ -18,30 +19,17 @@ model = tf.keras.models.load_model('model')
 model.summary()
 
 # %%
-#testing a bike
-object = {
-    0:   'bike',
-    1:   'motorcycle',
-    2:   'bus',
-    3:   'truck',
-    4:   'car',
-    5:   'train',
-    6:   'person',
-    7:   'traffic light',
-    8:   'stop sign',
-    9:   'fire hydrant',
-    10: 'background'
-    }
+
 x=0
 images=[]
 for data in tfds.as_numpy(tfds.load('coco').get('test2015')):
-    print(data)
+    #print(data)
     if(x>5):
         break
 
     image = Image.fromarray(data['image']) 
-    image.show()
-    print(data['image/filename'].decode("utf-8") )
+    #image.show()
+    #print(data['image/filename'].decode("utf-8") )
     images.append(image)
     x+=1
 # %%
@@ -56,11 +44,14 @@ crops_set = scale_and_slide.sliding_window(img, window_dimensions, stride)
 #rescaled_images = scale_and_slide.get_scaled_images(img, num_rescales, rescale_increment)
 scale_and_slide.simple_display_image(img)
 # %%
-
+data = params.chosen_labels
+data.append("background")
+data.sort()
+print(data)
 for i in crops_set:
-    
+    #print(i[0])
     #scale_and_slide.display_crops(rescaled_images[i], crops[i], window_dimensions, stride)
-    i= i.resize((64,64))
+    i= i[0].resize((100,100))
     plt.figure()
     
     
@@ -68,13 +59,16 @@ for i in crops_set:
     obj= tf.keras.preprocessing.image.img_to_array(i)
     obj = tf.expand_dims(obj, 0) # Create a batch
     predictions = model.predict(obj)
-    print(predictions.shape)
-    print(predictions)
+    # print(predictions.shape)
+    # print(predictions)
    
     score = tf.nn.softmax(predictions[0])
-    print(score)
+    #print(score)
+    
+    #print(obj)
+    
     plt.imshow(i)
-    plt.title((" {} with {:.2f} percent confidence.".format(np.argmax(score), 100 * np.max(score))))
+    plt.title((" {} with {:.2f} percent confidence.".format(data[np.argmax(score)], 100 * np.max(score))))
     plt.show()
     
 # %%
